9d9151bd8c6507d525f1e12d20ca0ecab926ce16
agonza42 <agonza42@nd.edu>
2023-10-31 21:41:27 -0400

    Updating Back4App Dashboard

--- src/environments.js
+++ src/environments.js
@@ -1,5 +1,5 @@
 module.exports = {
-  APPLICATION_ID: "gspWaVoJ9GgGLpXiJMsk0vJwZzbNiRPRH98mDAjj",
-  JAVASCRIPT_KEY: "oN4byqHU3kGgwubzpsFBs7bcvAFXqSbpKYzx6MJr",
+  APPLICATION_ID: "pnsSmalcjJOOZUC8FSXcfblEym4hBc5o271eeUJH",
+  JAVASCRIPT_KEY: "heFBiUwcSoU1xneJBLxuPDxRGouYE8h8I1wg1ed8",
   SERVER_URL: "https://parseapi.back4app.com/"
 };
