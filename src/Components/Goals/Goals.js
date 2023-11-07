4f88b2df189e456a8a9d9d9b4f9002e1015d2fd1
agonza42 <agonza42@nd.edu>
2023-11-06 21:17:18 -0500

    Reload Functionality on Goals

--- src/Components/Goals/Goals.js
+++ src/Components/Goals/Goals.js
@@ -1,61 +1,49 @@
-import React, { Component } from 'react';
-import { createGoalForm } from '../../Common/Services/GoalsService.js';
+import React, { useState } from 'react';
+import { createGoalForm } from '../../Common/Services/GoalsService';
+import { useNavigate } from 'react-router-dom';
 
 // Import the child component
 import GoalsChild from './GoalsChild';
 
 // Import the CSS file
-import '../../Style/Goals.css'; 
+import '../../Style/Goals.css';
 
-class Goals extends Component {
-  constructor() {
-    super();
-    this.state = {
-      formData: {
-        height: '',
-        weight: '',
-        age: '',
-        goal: '',
-      },
-    };
+function Goals() {
+  const [formData, setFormData] = useState({
+    height: '',
+    weight: '',
+    age: '',
+    goal: '',
+  });
 
-    // Binding event handlers to maintain the correct 'this' context
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+  // Initialize navigate
+  const navigate = useNavigate();
 
   // Function to handle events that change the input
-  handleInputChange(event) {
-    // Logging into the console to make sure the form is getting the correct values
-    console.log('Event Target:', event.target);
-    console.log('Name:', event.target.name);
-    console.log('Value:', event.target.value);
+  const handleInputChange = (event) => {
 
+    // Initializing name and value
     const { name, value } = event.target;
 
-    this.setState(prevState => ({
-        formData: {
-            ...prevState.formData,
-            [name]: name === 'age' || name === 'height' || name === 'weight' ? parseFloat(value) : value
-        }
-    }), () => {
-        console.log('Updated state:', this.state.formData);
-    });
-}
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: name === 'age' || name === 'height' || name === 'weight' ? parseFloat(value) : value,
+    }));
+  };
 
   // Function to handle asynchronous data when a submit event occurs
-  async handleSubmit(event) {
+  const handleSubmit = async (event) => {
+
     event.preventDefault();
-    console.log("Submitting form data:", this.state.formData);
     
-    // Parse the values
-    const heightValue = parseFloat(this.state.formData.height);
-    const weightValue = parseFloat(this.state.formData.weight);
-    const ageValue = parseInt(this.state.formData.age);
-    const goalValue = this.state.formData.goal;
+    // Get the formData and ensure proper variable types
+    const { height, weight, age, goal } = formData;
+    const heightValue = parseFloat(height);
+    const weightValue = parseFloat(weight);
+    const ageValue = parseInt(age);
 
     // Validate the values
-    if (isNaN(heightValue) || isNaN(weightValue) || isNaN(ageValue) || !goalValue) {
+    if (isNaN(heightValue) || isNaN(weightValue) || isNaN(ageValue) || !goal) {
       alert('Please provide valid input values.');
       return;
     }
@@ -65,42 +53,38 @@ class Goals extends Component {
       height: heightValue,
       weight: weightValue,
       age: ageValue,
-      goal: goalValue
+      goal
     };
 
     try {
-      // Create the goal using your service function
-      const response = await createGoalForm(goalData);
-      console.log('Goal created successfully:', response);
+      await createGoalForm(goalData);
       alert('Goal created successfully!');
-
+      // Navigate with 0 to reload the page and empty the form
+      navigate(0);
     } catch (error) {
       console.error('Error creating the goal:', error);
-      alert('Error creating the goal.')
+      alert('Error creating the goal.');
     }
-  }
+  };
 
-  // Using render for the component's HTML including form and button for user interaction
-  render() {
-    return (
-      <div>
-        <hr />
-        <h3>Personalized Fitness Goal-Setting</h3>
-        <p>
-          Please enter your information and goals for us to provide personalized
-          milestones and goals!
-        </p>
+  return (
+    <div>
+      <hr />
+      <h3>Personalized Fitness Goal-Setting</h3>
+      <p>
+        Please enter your information and goals for us to provide personalized
+        milestones and goals!
+      </p>
 
-        <form id="form" method="post" onSubmit={this.handleSubmit}>
-          <GoalsChild
-            formData={this.state.formData}
-            onChange={this.handleInputChange}
-          />
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-    );
-  }
+      <form id="form" method="post" onSubmit={handleSubmit}>
+        <GoalsChild
+          formData={formData}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default Goals;
