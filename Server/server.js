77b8bebdadba93cbc85d06423d1d8beb36c41a64
agonza42 <agonza42@nd.edu>
2023-12-01 17:50:43 -0600

    Recommendations Working w/ Node (MISSING HTML)

--- Server/server.js
+++ Server/server.js
@@ -17,6 +17,11 @@ const port = 4000;
 
 // Function to calculate BMR
 const calculateBMR = (weight, height, age) => {
+    if (isNaN(weight) || isNaN(height) || isNaN(age)) {
+        console.error('Invalid input for BMR calculation');
+        return NaN;
+    }
+    
     // Convert weight from kg to pounds and height from cm to inches for the calculation
     return 66.5 + (13.75 * weight) + (5.003 * height) - (6.75 * age);
 };
@@ -24,11 +29,6 @@ const calculateBMR = (weight, height, age) => {
 // NodeJS endpoint to get personalized recommendations
 app.get('/personalized-recommendations', async (req, res) => {
     const sessionToken = req.headers['x-parse-session-token'];
-
-    // Validate if the session token is provided
-    if (!sessionToken) {
-        return res.status(401).send('Session token is required.');
-    }
 
     try {
 
@@ -38,7 +38,7 @@ app.get('/personalized-recommendations', async (req, res) => {
         if (!currentUser) {
             return res.status(404).send('User not found.');
         }
-        console.log("User: ", currentUser);
+        //console.log("User: ", currentUser);
 
         // Query for the user's goal
         const Goal = Parse.Object.extend("Goal");
@@ -51,14 +51,14 @@ app.get('/personalized-recommendations', async (req, res) => {
             return res.status(404).send('Goal not found for user.');
         }
         const userGoal = userGoalObject.get('goal');
-        console.log("Goal: ", userGoal);
+        //console.log("Goal: ", userGoal);
 
         // Query for the user's tracking entries
         const TrackingEntry = Parse.Object.extend("TrackingEntry");
         const trackingQuery = new Parse.Query(TrackingEntry);
         trackingQuery.equalTo("user", currentUser);
         const trackingEntries = await trackingQuery.find({ sessionToken: sessionToken });
-        console.log("Entries: ", trackingEntries);
+        //console.log("Entries: ", trackingEntries);
 
         if (trackingEntries.length === 0) {
             return res.status(404).send('No tracking entries found for user.');
@@ -72,27 +72,40 @@ app.get('/personalized-recommendations', async (req, res) => {
             totalCalories += (foodCalories - exerciseCalories);
         });
         const averageCalories = trackingEntries.length > 0 ? totalCalories / trackingEntries.length : 0;
-        const bmr = calculateBMR(userGoalObject.get('weight'), userGoalObject.get('height'), currentUser.get('age'));
+
+
+        const userWeight = userGoalObject.get('weight');
+        const userHeight = userGoalObject.get('height');
+        const userAge = userGoalObject.get('age');
+
+        //console.log(`Weight: ${userWeight}, Height: ${userHeight}, Age: ${userAge}`);
+
+        const bmr = calculateBMR(Number(userWeight), Number(userHeight), Number(userAge));
+
+        //console.log("BMR: ", bmr);
+        //console.log("Cals: ", averageCalories);
 
         // Compare BMR with average calories to determine the case
         let caseResult;
-        if (userGoal === 'lose weight' && averageCalories < bmr) {
+        if (userGoal === 'lose' && averageCalories < bmr) {
             caseResult = 'loseWeightUnderBMR';
-        } else if (userGoal === 'lose weight' && averageCalories > bmr) {
+        } else if (userGoal === 'lose' && averageCalories > bmr) {
             caseResult = 'loseWeightOverBMR';
-        } else if (userGoal === 'gain weight' && averageCalories < bmr) {
+        } else if (userGoal === 'gain' && averageCalories < bmr) {
             caseResult = 'gainWeightUnderBMR';
-        } else if (userGoal === 'gain weight' && averageCalories > bmr) {
+        } else if (userGoal === 'gain' && averageCalories > bmr) {
             caseResult = 'gainWeightOverBMR';
-        } else if (userGoal === 'maintain weight') {
+        } else if (userGoal === 'maintain') {
             caseResult = 'maintainWeight';
-        } 
-        else {
-            caseResult = "missing"
         }
+        else {
+            caseResult = 'missing';
+        }
+        //console.log("Case: ", caseResult);
 
         // Send the recommendations back to the client
         res.json({ case: caseResult, bmr, averageCalories });
+        
     } catch (error) {
         console.error('Error while fetching user goals: ', error);
         res.status(500).send('An error occurred while processing data.');
