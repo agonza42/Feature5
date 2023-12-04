a5edf38f94f420ca71d8624e2f4bc086b6534864
agonza42 <agonza42@nd.edu>
2023-12-04 14:40:01 -0500

    Fixing Main

--- Client/src/Components/Main/Main.js
+++ Client/src/Components/Main/Main.js
@@ -15,31 +15,8 @@ const Main = () => {
       </h4>
 
       {/* IN PROGRESS: IMAGE CAROUSEL */}
-      <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
-        <ol className="carousel-indicators">
-          <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img className="d-block w-100" src="/Images/running.jpg" alt="First slide"></img>
-          </div>
-          <div className="carousel-item">
-            <img className="d-block w-100" src="/Images/pilates.jpg" alt="Second slide"></img>
-          </div>
-          <div className="carousel-item">
-            <img className="d-block w-100" src="/Images/weights.jpg" alt="Third slide"></img>
-          </div>
-        </div>
-        <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="sr-only">Previous</span>
-        </a>
-        <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="sr-only">Next</span>
-        </a>
+      <div id="staticImageContainer" className="static-slide">
+        <img className="d-block w-100" src="/Images/running.jpg" alt="Running"></img>
       </div>
     </div>
   );
